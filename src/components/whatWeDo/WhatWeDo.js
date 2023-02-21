3382f5bcfbfb83ac7e1d1801bec9e69a92e14a54
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-21 00:06:48 -0800

    styling

--- src/components/whatWeDo/WhatWeDo.js
+++ src/components/whatWeDo/WhatWeDo.js
@@ -15,14 +15,13 @@ const WhatWeDo = () => {
 
 
 
-
 <div className='wrapper'>
 <div class="wrap-divider-top">
 <img src={Wave2} loading="lazy" repeat-x alt='wave divider'/>
 </div>
 
 
-<div className='wrap'>
+<div className='whatwedo-wrap'>
 
 <Fade right>
     <div className="box-right">
@@ -31,11 +30,11 @@ const WhatWeDo = () => {
     </Fade>
     <Fade left>
     <div className="box-left">
-        <h1 className='header'>WHAT WE DO</h1>
+        <h1 className='header whatwedo-header'>WHAT WE DO</h1>
         <br/>
         <p>At Redding Rehab Treatment Center, we offer different levels of care based on each individual’s needs. Our programs consist of an intensive outpatient program (IOP) and a more demanding and structured partial hospitalization program (PHP). <br /> <br />You can be confident that our caring and professional staff will recommend the right level of care for you or your loved one.</p>
         <br />
-        <h1 className='header'>TREATMENT</h1>
+        <h1 className='header whatwedo-header'>TREATMENT</h1>
         <br/>
         <p>All treatment clients will have a recovery/treatment plan developed within 30 days of admission. Measurement and reassessment of treatment plan progress happen on a weekly basis through the weekly summary. <br/><br/> In the weekly summary, the drug and alcohol counselor will ensure that action steps to be taken by client or counselor are on target and that the action step achievements will enable resolution of the objective. All treatment plans are reviewed by and signed off on by the clinical director.</p>
     
